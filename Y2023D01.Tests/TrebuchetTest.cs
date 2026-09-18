c2708ee94cb0cdfb84920cea5c521bd59a064e32
Kristofer Sell <kristoferelissell@gmail.com>
2026-09-18 09:39:39 +0200

    Commented out test method

--- Y2023D01.Tests/TrebuchetTest.cs
+++ Y2023D01.Tests/TrebuchetTest.cs
@@ -11,23 +11,23 @@ public class TrebuchetTest
         var actual = sut.GetNumbers("1abc2");
         //Assert
         Assert.Equal(12, actual);
-        
+
     }
 
-    [Theory]
-    [InlineData("1abc2", 12)]
-    //[InlineData("pqr3stu8vwx", 38)]
-    //[InlineData("a1b2c3d4e5f", 15)]
-    //[InlineData("treb7uchet", 77)]
-    public void GetNumbers_FromText_ReturnValue(string text, int expected)
-    {
-        //Arrange
-        var sut = new Trebuchet();
-        //Act
-        var actual = sut.GetNumbers(text);
-        //Assert
-        Assert.Equal(expected, actual);
-    }
-    
-        
+    // [Theory]
+    // [InlineData("1abc2", 12)]
+    // [InlineData("pqr3stu8vwx", 38)]
+    // [InlineData("a1b2c3d4e5f", 15)]
+    // [InlineData("treb7uchet", 77)]
+    // public void GetNumbers_FromText_ReturnValue(string text, int expected)
+    // {
+    //     //Arrange
+    //     var sut = new Trebuchet();
+    //     //Act
+    //     var actual = sut.GetNumbers(text);
+    //     //Assert
+    //     Assert.Equal(expected, actual);
+    // }
+
+
 }
